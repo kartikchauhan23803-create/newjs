7f7e134093bf334a1e7519368b99d8f2519ef1b1
kartikchauhan23803-create <kartikchauhan23803@gmail.com>
2026-05-05 13:10:57 +0530

    first commit

---
+++ second.js
@@ -0,0 +1,11 @@
+a=50
+b=25
+c=a+b
+document.write(c);
+document.write("<br> minus is ",a-b);
+document.write("<br> multiply is ",a*b)
+document.write("<br> divide is ",a/b)
+document.write("<br> persentege is ",a%b)
+document.write("<br> increment is ",++a)
+document.write("<br> decrement is ",--b)
+alert("the value of c is "+c);
